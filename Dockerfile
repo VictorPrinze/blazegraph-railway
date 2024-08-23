14af9c167ab06a3168a0b0ae64bdb36fcc155f6b
git_VictorPrinze <kariukivictor226@gmail.com>
2024-08-23 18:15:08 +0300

    Update Blazegraph configuration for Render deployment

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,8 @@ RUN curl -L https://github.com/blazegraph/database/releases/download/BLAZEGRAPH_
 
 # Create necessary directories and set permissions
 RUN mkdir -p /data /logs && \
-    chown -R nobody:nogroup /data /logs && \
-    chmod 777 /data
+    chown -R nobody:nogroup /data /logs /opt/blazegraph && \
+    chmod 777 /data /logs
 
 # Copy configuration files
 COPY RWStore.properties /opt/blazegraph/
@@ -18,9 +18,6 @@ COPY log4j.properties /opt/blazegraph/
 # Copy startup script
 COPY start.sh /opt/blazegraph/
 RUN chmod +x /opt/blazegraph/start.sh
-
-# Set environment variable for timeout
-ENV BLAZEGRAPH_TIMEOUT=120
 
 # Switch to non-root user
 USER nobody
--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-set -e
+set -ex
 
 # Wait for the data directory to be mounted
 while [ ! -d "/data" ]; do
@@ -7,9 +7,29 @@ while [ ! -d "/data" ]; do
   sleep 1
 done
 
-# Start Blazegraph
-exec java -server -Xmx512m -Djetty.port=${PORT} \
+# Print directory contents and permissions
+ls -la /data
+ls -la /opt/blazegraph
+
+# Start Blazegraph with increased verbosity
+exec java -server -Xmx512m \
+  -Djetty.port=${PORT} \
   -Dcom.bigdata.journal.AbstractJournal.file=/data/blazegraph.jnl \
   -Dlog4j.configuration=file:/opt/blazegraph/log4j.properties \
-  -Dcom.bigdata.rdf.sail.webapp.ConfigParams.timeout=${BLAZEGRAPH_TIMEOUT} \
+  -Dcom.bigdata.rdf.sail.webapp.ConfigParams.timeout=300 \
+  -Dcom.bigdata.journal.AbstractJournal.bufferMode=DiskRW \
+  -Dcom.bigdata.journal.AbstractJournal.initialExtent=209715200 \
+  -Dcom.bigdata.journal.AbstractJournal.maximumExtent=209715200 \
+  -Dcom.bigdata.rdf.sail.truthMaintenance=false \
+  -Dcom.bigdata.rdf.store.AbstractTripleStore.quads=true \
+  -Dcom.bigdata.rdf.store.AbstractTripleStore.statementIdentifiers=false \
+  -Dcom.bigdata.rdf.store.AbstractTripleStore.textIndex=false \
+  -Dcom.bigdata.rdf.store.AbstractTripleStore.axiomsClass=com.bigdata.rdf.axioms.NoAxioms \
+  -Dorg.eclipse.jetty.server.Request.maxFormContentSize=10000000 \
+  -Dcom.bigdata.btree.writeRetentionQueue.capacity=4000 \
+  -Dcom.bigdata.btree.BTree.branchingFactor=128 \
+  -Dcom.bigdata.journal.Journal.groupCommit=true \
+  -verbose:gc \
+  -XX:+PrintGCDetails \
+  -XX:+PrintGCDateStamps \
   -jar blazegraph.jar
